67d11343b2167eb45f5041ddb3ac8afb14b9afa1
Joud <joudabihaidar2@gmail.com>
2024-03-14 21:26:16 +0200

    modules

---
+++ yahoo_finance_scraper.py
@@ -0,0 +1,22 @@
+#  Importing the WebDriver class for automated web browsing.
+from selenium import webdriver
+
+# Importing the Keys class for simulating keyboard inputs.
+from selenium.webdriver.common.keys import Keys
+
+# Importing the By class for locating elements on web pages.
+from selenium.webdriver.common.by import By
+
+# Importing the time module for adding time delays.
+import time
+
+# Importing the requests module for sending HTTP requests.
+import requests
+
+# Importing the BeautifulSoup class for parsing HTML and XML documents.
+from bs4 import BeautifulSoup
+
+# Importing the pandas library for data manipulation with the alias pd.
+import pandas as pd
+
+
