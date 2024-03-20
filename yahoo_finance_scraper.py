631198b840f3fe34171ddd426e516c6785bfaf6d
Joud <joudabihaidar2@gmail.com>
2024-03-20 19:19:06 +0200

    adding comments

--- yahoo_finance_scraper.py
+++ yahoo_finance_scraper.py
@@ -19,12 +19,20 @@ from bs4 import BeautifulSoup
 # Importing the pandas library for data manipulation with the alias pd.
 import pandas as pd
 
+# Concurrent task execution in python.
 import concurrent.futures
 
 url="https://finance.yahoo.com/quote/AAPL/news"
+
+"""
+Setting up a user-agent header to mimic a request coming from a specific web browser.
+This can help in scenarios where websites need to indentify the client's browser type,
+for compatibility or access purposes.
+"""
 headers={
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0'
 }
+
 allNews=[]
 
 def openWebPage(url):
@@ -58,36 +66,54 @@ def extractNews(driver):
 
         # The list of article elements:
         articlesList=soup.find('ul',{'class':'My(0) P(0) Wow(bw) Ov(h)'}).find_all('h3',{'class':'Mb(5px)'})
+
+        # for debugging:
         print(len(articlesList))
+
         driver.implicitly_wait(3)
     driver.quit()
     return articlesList
 
 def fetchNewsInfo(article):
+    """
+    Fetching the data of each article that we get from 'extractNews' function.
+    Getting the date, title, context, source_namme and link of each article.
+    """
+    # Printing the article's title for debugging
+    print(article.find('a').text)
+
+    # Full URL of the articles:
     link = 'https://finance.yahoo.com/quote/AAPL' + article.find('a')['href']
     
     r = requests.get(link, headers=headers)
+
+    # Parsing the HTML content of the article page:
     soup = BeautifulSoup(r.text, 'html.parser')
 
+    # Finding all the paragraphs of the article and concatenating them:
     articles = soup.find('div', {'class': 'caas-body'}).find_all('p')
-
     paragraph = ''
-
     for p in articles:
         paragraph += p.text
-    print(article.find('a').text)
+
+    # Dictionary containing all the data we want:
     news = {
         'Date': soup.find('time')['datetime'],
-        'article_title': article.find('a').text,  # Corrected line
+        'article_title': article.find('a').text,  
         'article': paragraph,
         'source_name': 'Yahoo Finance',
         'source_link': link
     }
 
+    # Global list allNews containing all the data about each article:
     allNews.append(news)
     return 
 
 def turnToCSV():
+    """
+    Converting the collected news data into a dataframe and then into a csv file,
+    while handling duplicates and cleaning the data.
+    """
     try:
         existing_data = pd.read_csv("News.csv")
     except FileNotFoundError:
@@ -102,6 +128,10 @@ def turnToCSV():
     df.to_csv("News.csv", index=False)
 
 def main():
+    """
+    Making the program run x100 faster by using concurrent execution,
+    to make the code fetch information from multiple news articles in parallel.
+    """
     with concurrent.futures.ThreadPoolExecutor() as executor:
         executor.map(fetchNewsInfo,extractNews(openWebPage(url)))
     turnToCSV()
