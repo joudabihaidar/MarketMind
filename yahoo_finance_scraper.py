15b84559f9c91d2f0fc2267dad80742f52a16b18
Joud <joudabihaidar2@gmail.com>
2024-03-21 13:01:28 +0200

    last change

--- yahoo_finance_scraper.py
+++ yahoo_finance_scraper.py
@@ -30,6 +30,7 @@ headers={
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0'
 }
 
+# Global list allNews that will contain all the data about each article:
 allNews=[]
 
 def openWebPage(url):
